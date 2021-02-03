e6cef997792bba9b125176d0ebbd8634371e9bb9
AIsmaili <achim.ismaili@telekom.de>
2021-02-03 22:46:34 +0100

    tried to add netlify cms as explained under https://www.netlifycms.org/docs/nextjs/

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,38 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import { Component } from "react";
+import { attributes, react as HomeContent } from "../content/home.md";
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default class Home extends Component {
+  render() {
+    let { title, cats } = attributes;
+    return (
+      <div className="container">
+        <Head>
+          <title>Next.js Starter!</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+        <main>
+          <Header title="Welcome to my app!" />
+          <h1>{title}</h1>
+          <p className="description">
+            Get started by editing <code>pages/index.js</code>!
+          </p>
+          <HomeContent />
+          <ul>
+            {cats.map((cat, k) => (
+              <li key={k}>
+                <h2>{cat.name}</h2>
+                <p>{cat.description}</p>
+              </li>
+            ))}
+          </ul>
+        </main>
 
-      <Footer />
-    </div>
-  )
+        <Footer />
+      </div>
+    )
+  }
 }
